a0e08ccc25f8b6c980b527a20f8c847a6d253306
maekawatatsuya1217 <tm.work.1217@gmail.com>
2022-03-13 19:04:40 +0900

    一覧表示ページ作成

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    // db接続
+    require 'mainte/db_connection.php';
+
+    // 情報取得
+    $sql = "SELECT * FROM blogs ORDER BY created_at DESC";
+
+    // 変数に代入
+    $blogs = $pdo->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Taskapp2</title>
+    </head>
+    <body>
+        <h1>一覧表示</h1>
+        <div class="new_btn">
+            <a href="create.php">新規投稿</a>
+        </div>
+        <!-- 一覧表示 -->
+        <?php foreach($blogs as $blog) : ?>
+            <div>
+                <h3><?php echo $blog['title'] ?></h3>
+                <p>筆者　<?php echo $blog['outhor_name'] ?></p>
+                <a href="#">編集</a>
+                <a href="#">削除</a>
+            </div>
+        <?php endforeach ?>
+        <!-- 一覧表示 -->
+    </body>
+</html>
